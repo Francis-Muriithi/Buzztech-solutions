a8c6ef0e660112569e0aaa400088402a66845691
Francis <mufraggie@gmail.com>
2022-08-08 17:12:23 +0300

    first edit

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
+h2.textContent = "About Us";
 document.querySelector("body").appendChild(h2);
+
+const p =document.createElement("p");
+p.textContent = "Kiremani Business Solutions is a futuristic company focused on resolving and re-adjusting business expectations basedd on current and future industry trends. We work with different entrepreneurs, innovators and partners to ensure that your busibess makes the best use of your available resources, opportunities and strengths";
+document.querySelector ("h2").appendChild(p);
+
+const img = document.createElement("img");
+img.src ="https://i0.wp.com/netlitdigital.com/wp-content/uploads/2020/06/business-solutions-Netlit-Digital-best-website-design-company-in-kenyad.jpg?w=1000&ssl=1";
+document.querySelector("h2").appendChild(img);
+
+const h22 = document.createElement("h22");
+h22.textContent = "What Solutions do we Offer?";
+document.querySelector("body").appendChild(h22);
+
+const p2 =document.createElement("p");
+p2.textContent = "The World of business is changing and our best solutions focus on ensuring that you are not left behind. We always begin by exploring your desired mission and vision. Taking charge of your dream helps us to understand better the direction your business has already taken and your aspirations for the future. This means that we always start with helping you choose the right form of business, and the best brand. Our exposure in the various industries in the country and beyond helps us to guide you in identifying the various digital & Offline Marketing Channels Suitable for your brand, Implementing an efficient and worthy Tax & Accounting System and even offer you Virtual Assistant Services. Now you can run a successful business with fewer challenges";
+document.querySelector ("h22").appendChild(p2);
+
